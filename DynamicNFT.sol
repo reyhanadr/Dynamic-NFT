68937975f60215442c4a0e5ecb2d598b2b08173c
reyhanadr <reyhan.a.deris@gmail.com>
2025-02-13 16:39:13 +0700

    first commit

---
+++ DynamicNFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MintMate is ERC721URIStorage {
+    uint256 public _tokenIdCounter;
+
+    constructor() ERC721("Dynamic NFT", "Dynamic NFT Ethereum Sepolia") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 tokenId = _tokenIdCounter;
+        _mint(recipient, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        _tokenIdCounter += 1;
+
+        return tokenId;
+    }
+}
